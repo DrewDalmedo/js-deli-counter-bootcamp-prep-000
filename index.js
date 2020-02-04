6a1e4902ef7af4cbf74d38dbff90695cfc899930
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-04 18:09:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ var deliLine = [];
 
 function takeANumber(line, name) {
   line.push(name);
-  console.log(`Welcome, ${name}. You are number ${line.length} in line.`);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
+}
+
+function nowServing(line) {
+  return line.unshift();
+}
+
+function currentLine(line) {
+  
 
 }
